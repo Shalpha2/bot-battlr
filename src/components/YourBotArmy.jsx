ea414fbe2296f62b6f831d6c650387c7bc56fcf8
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-04-19 03:36:20 -0700

    set

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -3,22 +3,26 @@ import BotCard from "./BotCard";
 
 function YourBotArmy({ army, onRelease, onDischarge }) {
   return (
-    <div className="my-5">
-      <h2 className="text-center my-4">Your Bot Army</h2>
-      <div className="row">
+    <div className="container my-2">
+    <div className="bg-lime p-1 mb-2 rounded">
+  <h3 className="text-center">🤖 Your Bot Army</h3>
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         {army.map((bot) => (
-          <div key={bot.id} className="col-md-3 mb-4">
+          <div key={bot.id} className="col">
             <BotCard
               bot={bot}
               onClick={() => onRelease(bot)}
               onDischarge={() => onDischarge(bot)}
-              showDischarge
+              showDischarge={true}
             />
           </div>
         ))}
       </div>
     </div>
+    </div>
   );
 }
 
 export default YourBotArmy;
+
+
